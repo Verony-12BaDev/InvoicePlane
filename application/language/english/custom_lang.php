de4fde16e6b3ae5215f26db48dd500886e033ccd
Verony <Verony.12BaDev@gmail.com>
2018-01-28 20:32:21 +0100

    Client UBL selection
    User can select the UBL/Zugferd xml invoice format based on the client wish.

--- application/language/english/custom_lang.php
+++ application/language/english/custom_lang.php
@@ -13,5 +13,17 @@
  */
 
 $lang = array(
-
+    'bank'                                         => 'Bank',
+    'contact'                                      => 'Contact',
+    'information'                                  => 'information',            
+    'invoicing'                                    => 'Invoicing',
+    'ubl_select_help'                              => 'Select the correct UBL version for your customer or his country. The application will create a Pdf and XML-file.',   
+    'version'                                      => 'version',
+    'view_pdf'                                     => 'View Pdf', 
+    'client_sepa_ct'                               => 'SEPA Credit Transfer',
+    'client_sepa_ct_help'                          => 'Create a SEPA Credit Transfer file and send this to the customer.<br>If used then IBAN is mandatory and Bank and BIC are recommended!',     
+    'user_ical'                                    => 'Send calendar reminder',
+    'user_ical_desc'                               => 'URGENT! Your sales invoice will expire today. Check ASAP that this invoice has already been paid.',
+    'user_ical_help'                               => 'Also send a (calendar) expiry reminder to the email address of the user, with each invoice.',
+    'user_ical_sum'                                => 'Reminder invoice',
 );
